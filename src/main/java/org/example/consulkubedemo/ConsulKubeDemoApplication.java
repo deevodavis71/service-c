777efd7b47ea4c68483b5a398898049819885a6d
hariinfo <hariinfo@gmail.com>
2018-04-14 08:22:07 -0500

    first commit

---
+++ src/main/java/org/example/consulkubedemo/ConsulKubeDemoApplication.java
@@ -0,0 +1,12 @@
+package org.example.consulkubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulKubeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulKubeDemoApplication.class, args);
+	}
+}
